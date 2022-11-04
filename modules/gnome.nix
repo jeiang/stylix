68f06130465775912f42faa892671838556ad99a
Aidan Pinard <aidan@aidanpinard.co>
2022-11-04 09:38:42 -0400

    Had the order backwards (facepalm emoji)
    Signed-off-by: Aidan Pinard <aidan@aidanpinard.co>

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -6,15 +6,7 @@
 
   config.home-manager.sharedModules =
     if config.stylix.polarity == "dark"
-    then 
-      lib.mkIf config.stylix.targets.gnome.enable [{
-        dconf.settings."org/gnome/desktop/background" = {
-          color-shading-type = "solid";
-          picture-options = "zoom";
-          picture-uri = "file://${config.stylix.image}";
-        };
-      }]
-    else
+    then
       lib.mkIf config.stylix.targets.gnome.enable [{
         dconf.settings."org/gnome/desktop/background" = {
           color-shading-type = "solid";
@@ -22,8 +14,15 @@
           picture-uri-dark = "file://${config.stylix.image}";
         };
         dconf.settings."org/gnome/desktop/interface" = {
-          color-scheme="prefer-dark";
+          color-scheme = "prefer-dark";
+        };
+      }]
+    else
+      lib.mkIf config.stylix.targets.gnome.enable [{
+        dconf.settings."org/gnome/desktop/background" = {
+          color-shading-type = "solid";
+          picture-options = "zoom";
+          picture-uri = "file://${config.stylix.image}";
         };
       }];
-    
 }
